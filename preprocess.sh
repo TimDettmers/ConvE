9e77da660b877aa5e584e2b35bb4a5beafcbb421
Tim Dettmers <tim.dettmers@gmail.com>
2017-10-24 11:59:58 +0100

    Added UMLS, Kinship, and Nations datasets; see https://arxiv.org/abs/1705.11040;

--- preprocess.sh
+++ preprocess.sh
@@ -3,10 +3,19 @@ mkdir data
 mkdir data/WN18RR
 mkdir data/YAGO3-10
 mkdir data/FB15k-237
+mkdir data/nations
+mkdir data/kinship
+mkdir data/umls
 mkdir saved_models
 tar -xvf WN18RR.tar.gz -C data/WN18RR
 tar -xvf YAGO3-10.tar.gz -C data/YAGO3-10
 tar -xvf FB15k-237.tar.gz -C data/FB15k-237
+tar -xvf umls.tar.gz -C data/umls
+tar -xvf kinship.tar.gz -C data/kinship
+tar -xvf nations.tar.gz -C data/nations
 python wrangle_KG.py WN18RR
 python wrangle_KG.py YAGO3-10
 python wrangle_KG.py FB15k-237
+python wrangle_KG.py umls
+python wrangle_KG.py nations
+python wrangle_KG.py kinship
